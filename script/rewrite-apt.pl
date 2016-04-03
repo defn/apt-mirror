760d240f7b2c960f411eff07896d3bac9c3901c4
Tom Bombadil <amanibhavam@destructuring.org>
2016-04-02 18:07:42 -0700

    rewrite url with proper options

--- script/rewrite-apt.pl
+++ script/rewrite-apt.pl
@@ -5,11 +5,12 @@ use strict;
 $| = 1;
 
 while (<>) {
-    my @line = split;
-    $_ = $line[0];
-    if (m{^(https?)://[^/]*archive.ubuntu.com/(.*)}) {
-        print "$1://172.28.128.1:3000/$2\n";
-    } else {
-        print $_ . "\n";
-    }
+  my @line = split;
+  $_ = $line[1];
+  if (m{^(https?)://archive.ubuntu.com/(.*)}) {
+    print "OK rewrite-url=\"http://172.28.128.1:3000/$2\"\n";
+  } 
+  else {
+    print "$line[0]\n";
+  }
 }
