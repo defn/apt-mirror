6f6b863751f1e95e20f9dd4c6311b04dad674f93
Tom Bombadil <amanibhavam@destructuring.org>
2016-04-02 20:23:26 -0700

    fix capture group

--- script/rewrite-apt.pl
+++ script/rewrite-apt.pl
@@ -6,11 +6,12 @@ $| = 1;
 
 while (<>) {
   my @line = split;
-  $_ = $line[1];
-  if (m{^(https?)://archive.ubuntu.com/(.*)}) {
-    print "OK rewrite-url=\"http://172.28.128.1:3000/$2\"\n";
+  $_ = $line[0];
+  if (m{^(https?)://[^/]*(archive|security).ubuntu.com/(.*)}) {
+    print "OK rewrite-url=\"http://172.28.128.1:3000/$3\"\n";
   } 
   else {
     print "$line[0]\n";
   }
 }
+
