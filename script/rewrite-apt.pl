2534c5e566a18b7bbd60fe759be9a7931b57ad95
Tom Bombadil <amanibhavam@destructuring.org>
2016-06-20 22:40:25 -0700

    redirect to cache vip

--- script/rewrite-apt.pl
+++ script/rewrite-apt.pl
@@ -8,7 +8,7 @@ while (<>) {
   my @line = split;
   $_ = $line[0];
   if (m{^(https?)://[^/]*(archive|security).ubuntu.com/(.*)}) {
-    print "OK rewrite-url=\"http://172.28.128.1:3000/$3\"\n";
+    print "OK rewrite-url=\"$ENV{'CACHE_VIP'}:3000/$3\"\n";
   } 
   else {
     print "$line[0]\n";
