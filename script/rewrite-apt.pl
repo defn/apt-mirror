90035a9d1ed5e1595db8af76df955914f41eae7f
Tom Bombadil <amanibhavam@destructuring.org>
2016-06-23 12:52:45 -0700

    dont use apt-cache if mirror isnt there

--- script/rewrite-apt.pl
+++ script/rewrite-apt.pl
@@ -7,11 +7,14 @@ $| = 1;
 while (<>) {
   my @line = split;
   $_ = $line[0];
-  if (m{^(https?)://[^/]*(archive|security).ubuntu.com/(.*)}) {
-    print "OK rewrite-url=\"$ENV{'CACHE_VIP'}:3000/$3\"\n";
-  } 
-  else {
-    print "$line[0]\n";
+
+  if (-d "$ENV{'CACHE_DIR'}/cache/apt-mirror/mirror/archive.ubuntu.com") {
+    if (m{^(https?)://[^/]*(archive|security).ubuntu.com/(.*)}) {
+      print "OK rewrite-url=\"$ENV{'CACHE_VIP'}:3000/$3\"\n";
+      next;
+    } 
   }
+
+  print "$line[0]\n";
 }
 
